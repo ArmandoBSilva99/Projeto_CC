25a42aa488cb7cf59f21d460ce6948bbbdcc5d28
ArmandoBSilva99 <armandob.silva99@gmail.com>
2021-12-16 09:38:50 +0000

    more stuff

--- src/FTRapidProtocol.java
+++ src/FTRapidProtocol.java
@@ -6,53 +6,6 @@ import java.nio.charset.StandardCharsets;
 
 public class FTRapidProtocol {
 
-    //Probably inutil?
-    /*public void start(DatagramSocket s, String hostS, int port, String message) throws IOException, DateTimeParseException{
-        InetAddress address = s.getLocalAddress();
-        HostInfo f = new HostInfo(port, address.getHostAddress());
-        //String message = f.myFiles();
-        //Map<String,FileInfo> myInfo = parse(message);
-        /*Map<String,FileInfo> map = f.parse(f.myFiles());
-        for (Map.Entry<String, FileInfo> infos : map.entrySet()){
-            FileInfo fi = infos.getValue();
-            System.out.println("Nome: " + fi.getName() + " Data: " + fi.getData());
-        }
-        //
-        byte[] m = message.getBytes();
-        SimplePacket p = new SimplePacket(0,0,m); 
-        send(s, hostS, port, p);
-        System.out.println("sent Servidor1");
-        s.setSoTimeout(10000);
-
-        SimplePacket ack = receive(s);
-        //readMessageType(ack.getMsgType(),ack);
-        //Mensagem recebida
-        byte[] recv = ack.getData();
-        String sRecv = new String(recv, StandardCharsets.UTF_8);
-        System.out.println("Mensagem recebida: " + sRecv);
-        //Map<String,FileInfo> otherInfo = parse(sRecv);
-
-        System.out.println("Servidor 1 diz: " + ack.getAck());
-        if (ack.getAck() == 0) {
-            p.setAck(1);
-            System.out.println("Not Acknowledged\nSending again...");
-            send(s, hostS, port, p);
-
-            System.out.println("Connection Established");
-
-        }
-        printSimplePacket(ack);
-
-        SimplePacket pack = missingFiles(message,sRecv);  
-        send(s, hostS, port, pack);
-
-        SimplePacket ans = receive(s);
-        printSimplePacket(ans);
-
-        //send File...
-    }
-    */
-
     //To establish connection
     public void connCheck(DatagramSocket s, String hostS, int port, SimplePacket p, SimplePacket ack) throws IOException{
         System.out.println("Servidor 1 diz: " + ack.getAck());
